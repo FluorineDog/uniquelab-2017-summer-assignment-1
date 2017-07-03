35bff5e003c9e2305b442c80ba1a92751cb5ba8c
klx3300 <inc19991@gmail.com>
2017-07-03 14:31:02 +0800

    some testcases for pq

--- pq_test.cpp
+++ pq_test.cpp
@@ -25,4 +25,50 @@ namespace {
     EXPECT_EQ(std_pq.top(), pq.top());
     EXPECT_EQ(std_pq.size(), pq.size());
   }
+  TEST(push,ordered_data)
+  {
+    std::priority_queue<double> std_pq;
+    Priority_queue nonstd_pq;
+    for(unsigned int i=0;i<10000;i++){
+      std_pq.push((double)i);
+      nonstd_pq.push((double)i);
+    }
+    EXPECT_EQ(std_pq.top(),nonstd_pq.top());
+    EXPECT_EQ(std_pq.size(),nonstd_pq.size());
+  }
+  TEST(push,repeated_data,ordered_data){
+    std::priority_queue<double> std_pq;
+    Priority_queue nonstd_pq;
+    for(unsigned int i=0;i<10000;i++){
+      std_pq.push((double)i);
+      nonstd_pq.push((double)i);
+      std_pq.push(0.0f);
+      nonstd_pq.push(0.0f);
+    }
+    EXPECT_EQ(std_pq.top(),nonstd_pq.top());
+    EXPECT_EQ(std_pq.size(),nonstd_pq.top());
+    for(int i=0;i<20000;i++){
+      EXPECT_EQ(std_pq.top(),nonstd_pq.top());
+      std_pq.pop();
+      nonstd_pq.pop();
+    }
+  }
+  TEST(push,crossing_data){
+    std::priority_queue<double> std_pq;
+    Priority_queue nonstd_pq;
+    for(unsigned int i=0;i<10000;i++){
+      std_pq.push((double)i);
+      nonstd_pq.push((double)i);
+      std_pq.push((double)-i);
+      nonstd_pq.push((double)-i);
+    }
+    EXPECT_EQ(std_pq.size(),nonstd_pq.size())
+  EXPECT_EQ(std_pq.top(),nonstd_pq.top())
+  for(int i=0;i<20000;i++){
+      EXPECT_EQ(std_pq.top(),nonstd_pq.top());
+      std_pq.pop();
+      nonstd_pq.pop();
+  }
+  }
+  
 }
