de5b0dde8c01908958774ad17993fcf21346aaab
Kyler KY <kylerky@live.com>
2017-07-03 14:25:51 +0800

    set a constant seed

--- set_test.cpp
+++ set_test.cpp
@@ -7,15 +7,14 @@
 namespace {
   TEST(push, random_large)
   {
-    std::random_device rand_dev;
 
-    std::default_random_engine e(rand_dev());
+    std::default_random_engine e(810);
     std::uniform_real_distribution<double> dis(0, 100);
 
     std::set<double> std_set;
     Set custom_set;
 
-    for (unsigned i = 0; i != 10000)
+    for (unsigned i = 0; i != 10000; ++i)
     {
       double num = dis(e);
       std_set.insert(num);
