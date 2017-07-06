f3de0c600e46c67d9f77544d05f6b014bd0e47a0
qzwlecr <qzwlecr@gmail.com>
2017-07-06 13:17:37 +0800

    try to add more test

--- pq_test.cpp
+++ pq_test.cpp
@@ -4,9 +4,20 @@
 
 #include "priority_queue.h"
 
-namespace {
-  TEST(push, random_large)
-  {
+class  priorityQueueTest: public testing::Test {
+protected:
+    virtual void SetUp() {
+        start_time = time(NULL);
+    }
+
+    virtual void TearDown() {
+        time_t end_time = time(NULL);
+        EXPECT_TRUE(end_time-start_time <=10)<< "The test took too long.";
+    }
+    time_t start_time;
+};
+
+TEST_F(priorityQueueTest, random_small) {
     std::random_device rand_dev;
 
     std::default_random_engine e(810);
@@ -15,61 +26,139 @@ namespace {
     std::priority_queue<double> std_pq;
     Priority_queue pq;
 
-    for (unsigned i = 0; i != 1000000; ++i)
-    {
-      double num = dis(e);
-      std_pq.push(num);
-      pq.push(num);
+    for (unsigned i = 0; i != 100; ++i) {
+        double num = dis(e);
+        std_pq.push(num);
+        pq.push(num);
     }
 
-    EXPECT_EQ(std_pq.top(), pq.top());
-    EXPECT_EQ(std_pq.size(), pq.size());
-  }
-  TEST(push,ordered_data)
-  {
+    ASSERT_DOUBLE_EQ(std_pq.top(), pq.top());
+    ASSERT_EQ(std_pq.size(), pq.size());
+}
+
+TEST_F(priorityQueueTest,random_large) {
+    std::random_device rand_dev;
+
+    std::default_random_engine e(810);
+    std::uniform_real_distribution<double> dis(0, 100);
+
     std::priority_queue<double> std_pq;
-    Priority_queue nonstd_pq;
-    for(unsigned int i=0;i<10000;i++){
-      std_pq.push((double)i);
-      nonstd_pq.push((double)i);
+    Priority_queue pq;
+
+    for (unsigned i = 0; i != 5000000; ++i) {
+        double num = dis(e);
+        std_pq.push(num);
+        pq.push(num);
     }
-    EXPECT_EQ(std_pq.top(),nonstd_pq.top());
-    EXPECT_EQ(std_pq.size(),nonstd_pq.size());
-  }
-  TEST(push,repeated_data){
+    ASSERT_DOUBLE_EQ(std_pq.top(), pq.top());
+    ASSERT_EQ(std_pq.size(), pq.size());
+}
+
+TEST_F(priorityQueueTest, only_data) {
     std::priority_queue<double> std_pq;
-    Priority_queue nonstd_pq;
-    for(unsigned int i=0;i<10000;i++){
-      std_pq.push((double)i);
-      nonstd_pq.push((double)i);
-      std_pq.push(0.0f);
-      nonstd_pq.push(0.0f);
-      EXPECT_EQ(std_pq.top(),nonstd_pq.top());    
+    Priority_queue pq;
+    for (unsigned int i = 0; i != 100000; ++i) {
+        std_pq.push((double) 1);
+        pq.push((double) 1);
     }
-    EXPECT_EQ(std_pq.size(),nonstd_pq.size());
-    for(int i=0;i<20000;i++){
-      EXPECT_EQ(std_pq.top(),nonstd_pq.top());
-      std_pq.pop();
-      nonstd_pq.pop();
-    }
-  }
-  TEST(push,crossing_data){
-    std::priority_queue<double> std_pq;
-    Priority_queue nonstd_pq;
-    for(unsigned int i=0;i<10000;i++){
-      std_pq.push((double)i);
-      nonstd_pq.push((double)i);
-      std_pq.push((double)-i);
-      nonstd_pq.push((double)-i);
-      EXPECT_EQ(std_pq.top(),nonstd_pq.top());
-      
-    }
-    EXPECT_EQ(std_pq.size(),nonstd_pq.size());
-    for(int i=0;i<20000;i++){
-        EXPECT_EQ(std_pq.top(),nonstd_pq.top());
+    ASSERT_DOUBLE_EQ(std_pq.top(), pq.top());
+    ASSERT_EQ(std_pq.size(), pq.size());
+    while (!pq.empty()) {
+        ASSERT_DOUBLE_EQ(std_pq.top(), pq.top());
         std_pq.pop();
-        nonstd_pq.pop();
+        pq.pop();
     }
-  }
-  
+    ASSERT_EQ(pq.size(), 0);
+    ASSERT_EQ(std_pq.size(), 0);
+
+}
+
+TEST_F(priorityQueueTest, ordered_data) {
+    std::priority_queue<double> std_pq;
+    Priority_queue pq;
+    for (unsigned int i = 0; i != 100000; ++i) {
+        std_pq.push((double) i);
+        pq.push((double) i);
+    }
+    ASSERT_DOUBLE_EQ(std_pq.top(), pq.top());
+    ASSERT_EQ(std_pq.size(), pq.size());
+    while (!pq.empty()) {
+        ASSERT_DOUBLE_EQ(std_pq.top(), pq.top());
+        std_pq.pop();
+        pq.pop();
+    }
+    ASSERT_EQ(pq.size(), 0);
+    ASSERT_EQ(std_pq.size(), 0);
+}
+
+TEST_F(priorityQueueTest, repeated_data) {
+    std::priority_queue<double> std_pq;
+    Priority_queue pq;
+    for (unsigned int i = 0; i != 100000; ++i) {
+        std_pq.push((double) i);
+        pq.push((double) i);
+        std_pq.push((double) (i + 1));
+        pq.push((double) (i + 1));
+        ASSERT_DOUBLE_EQ(std_pq.top(), pq.top());
+        ASSERT_EQ(std_pq.size(), pq.size());
+    }
+    ASSERT_EQ(std_pq.size(), pq.size());
+    while (!pq.empty()) {
+        ASSERT_DOUBLE_EQ(std_pq.top(), pq.top());
+        std_pq.pop();
+        pq.pop();
+    }
+    ASSERT_EQ(pq.size(), 0);
+    ASSERT_EQ(std_pq.size(), 0);
+}
+
+TEST_F(priorityQueueTest, crossing_data) {
+    std::priority_queue<double> std_pq;
+    Priority_queue pq;
+    for (int i = 0; i != 100000; ++i) {
+        std_pq.push((double) i);
+        pq.push((double) i);
+        std_pq.push((double) -i);
+        pq.push((double) -i);
+        ASSERT_DOUBLE_EQ(std_pq.top(), pq.top());
+        ASSERT_EQ(std_pq.size(), pq.size());
+    }
+    ASSERT_EQ(std_pq.size(), pq.size());
+    while (!pq.empty()) {
+        ASSERT_DOUBLE_EQ(std_pq.top(), pq.top());
+        std_pq.pop();
+        pq.pop();
+    }
+    ASSERT_EQ(pq.size(), 0);
+    ASSERT_EQ(std_pq.size(), 0);
+}
+
+TEST_F(priorityQueueTest, random) {
+    std::random_device rand_dev;
+
+    std::default_random_engine e(810);
+    std::uniform_real_distribution<double> dis(0, 100);
+
+    std::priority_queue<double> std_pq;
+    Priority_queue pq;
+    for (unsigned times = 0; times < 10; ++times) {
+        for (unsigned i = 0; i != 100000; ++i) {
+            double num = dis(e);
+            std_pq.push(num);
+            pq.push(num);
+        }
+
+        ASSERT_DOUBLE_EQ(std_pq.top(), pq.top());
+        ASSERT_EQ(std_pq.size(), pq.size());
+
+        while (!std_pq.empty()) {
+            ASSERT_DOUBLE_EQ(std_pq.top(), pq.top());
+            ASSERT_EQ(std_pq.size(), pq.size());
+            std_pq.pop();
+            pq.pop();
+        }
+
+        ASSERT_EQ(pq.size(), 0);
+        ASSERT_EQ(std_pq.size(), 0);
+    }
 }
--- set_test.cpp
+++ set_test.cpp
@@ -2,11 +2,23 @@
 #include <random>
 #include "include/gtest/gtest.h"
 
-#include "set"
+#include "set.h"
 
-namespace {
-  TEST(push, random_large)
-  {
+class setTest : public testing::Test {
+protected:
+    virtual void SetUp() {
+        start_time = time(NULL);
+    }
+
+    virtual void TearDown() {
+        time_t end_time = time(NULL);
+        ASSERT_TRUE(end_time - start_time <= 10) << "The test took too long.";
+    }
+
+    time_t start_time;
+};
+
+TEST_F(setTest, random_small) {
 
     std::default_random_engine e(810);
     std::uniform_real_distribution<double> dis(0, 100);
@@ -14,13 +26,136 @@ namespace {
     std::set<double> std_set;
     Set custom_set;
 
-    for (unsigned i = 0; i != 10000; ++i)
-    {
-      double num = dis(e);
-      std_set.insert(num);
-      custom_set.insert(num);
+    for (unsigned i = 0; i != 100; ++i) {
+        double num = dis(e);
+        std_set.insert(num);
+        custom_set.insert(num);
     }
 
-    EXPECT_EQ(std_set.size(), custom_set.size());
-  }
+    ASSERT_EQ(std_set.size(), custom_set.size());
+    for (auto s: std_set) {
+        ASSERT_EQ(custom_set.count(s), 1);
+        custom_set.erase(s);
+    }
+    ASSERT_EQ(custom_set.size(), 0);
+}
+
+TEST_F(setTest, random_large) {
+
+    std::default_random_engine e(810);
+    std::uniform_real_distribution<double> dis(0, 100);
+
+    std::set<double> std_set;
+    Set custom_set;
+
+    for (unsigned i = 0; i != 500000; ++i) {
+        double num = dis(e);
+        std_set.insert(num);
+        custom_set.insert(num);
+    }
+
+    ASSERT_EQ(std_set.size(), custom_set.size());
+    for (auto s: std_set) {
+        ASSERT_EQ(custom_set.count(s), 1);
+        custom_set.erase(s);
+    }
+    ASSERT_EQ(custom_set.size(), 0);
+}
+
+TEST_F(setTest, only_data) {
+    std::set<double> std_set;
+    Set custom_set;
+    for (unsigned int i = 0; i != 100000; ++i) {
+        std_set.insert((double) 1);
+        custom_set.insert((double) 1);
+    }
+    ASSERT_EQ(std_set.size(), custom_set.size());
+    ASSERT_EQ(custom_set.count((double) 1), 1);
+}
+
+TEST_F(setTest, ordered_data) {
+    std::set<double> std_set;
+    Set custom_set;
+    for (unsigned int i = 0; i != 100000; ++i) {
+        std_set.insert((double) i);
+        custom_set.insert((double) i);
+        ASSERT_EQ(custom_set.count((double) i), std_set.count((double) i));
+    }
+    ASSERT_EQ(std_set.size(), custom_set.size());
+    for (auto s:std_set) {
+        custom_set.erase(s);
+    }
+    ASSERT_EQ(custom_set.size(), 0);
+}
+
+TEST_F(setTest, repeated_data) {
+    std::set<double> std_set;
+    Set custom_set;
+    for (unsigned int i = 0; i != 100000; ++i) {
+        std_set.insert((double) i);
+        std_set.insert((double) (i + 1));
+        custom_set.insert((double) i);
+        custom_set.insert((double) (i + 1));
+        ASSERT_EQ(custom_set.count((double) i), std_set.count((double) i));
+    }
+    ASSERT_EQ(std_set.size(), custom_set.size());
+    for (auto s:std_set) {
+        custom_set.erase(s);
+    }
+    ASSERT_EQ(custom_set.size(), 0);
+}
+
+TEST_F(setTest, crossing_data) {
+    std::set<double> std_set;
+    Set custom_set;
+    for (int i = 1; i != 100000; ++i) {
+        auto num = (long long)rand()*100000;
+        num %=i;
+        std_set.insert((double) i);
+        std_set.insert((double) (-i));
+        std_set.erase((double)(num));
+        custom_set.insert((double) i);
+        custom_set.insert((double) (-i));
+        custom_set.erase((double)(num));
+        ASSERT_EQ(custom_set.count((double) i), std_set.count((double) i));
+        ASSERT_EQ(custom_set.count((double) (-i)), std_set.count((double) (-i)));
+    }
+    ASSERT_EQ(std_set.size(), custom_set.size());
+    for (auto s:std_set) {
+        custom_set.erase(s);
+    }
+    ASSERT_EQ(custom_set.size(), 0);
+}
+
+TEST_F(setTest, random) {
+    std::random_device rand_dev;
+
+    std::default_random_engine e(810);
+    std::uniform_real_distribution<double> dis(0, 100);
+
+    std::set<double> std_set;
+    Set custom_set;
+    for (unsigned times = 0; times < 10; ++times) {
+        for (unsigned i = 0; i != 80000; ++i) {
+            double num = dis(e);
+            std_set.insert(num);
+            custom_set.insert(num);
+        }
+
+        ASSERT_EQ(std_set.size(), custom_set.size());
+
+        if (times % 2) {
+            for (auto s:std_set) {
+                custom_set.erase(s);
+                ASSERT_EQ(custom_set.count(s), 0);
+            }
+            std_set.clear();
+        } else {
+            custom_set.clear();
+            std_set.clear();
+        }
+
+        ASSERT_EQ(std_set.size(), 0);
+        ASSERT_EQ(custom_set.size(), 0);
+    }
 }
